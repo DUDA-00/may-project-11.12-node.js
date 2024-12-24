e5dff31dfee54580c2737d9efb9cacf8da94c9ac
dyda <ivanduda102@gmail.com>
2024-12-24 03:54:20 +0200

    hw-3

--- main.js
+++ main.js
@@ -1,42 +1,125 @@
-const path = require('node:path');
-const fs = require('node:fs/promises');
-
-const foo = async () => {
-
-    const pathToFile = path.join(__dirname, 'baseFolder');
-    await fs.mkdir(pathToFile, {recursive: true});
-
-     const folders = ['folder1','folder2','folder3','folder4','folder5'];
-     const files = ['file.txt1','file.txt2','file.txt3','file.txt4','file.txt5'];
-
-    await Promise.all(folders.map(async folder => {
-        const folderPath = path.join(pathToFile, folder);
-        await fs.mkdir(folderPath, { recursive: true });
-
-        await Promise.all(files.map(async file => {
-            await fs.writeFile(path.join(folderPath, file), 'Hello World!');
-
-        }))
-    }));
+const express = require('express')
+const dotenv = require('dotenv')
+const {read,write} = require('./fs.service.js');
+const {json} = require("express");
+dotenv.config()
+const app = express();
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 
 
-    const data = await fs.readdir(pathToFile, );
-    for (const folder of data) {
-        const folderPath = path.join(pathToFile, folder);
-        const files =  await fs.readdir(folderPath);
-        const stat =  await fs.stat(folderPath);
-        console.log(` File:${folderPath} - Is File: ${stat.isFile()}`);
 
-        for (const file  of files) {
-            const filePath = path.join(folderPath, file);
-            const stat =  await fs.stat(filePath);
-            console.log(`File: ${filePath} - Is File: ${stat.isFile()}`);
+app.get('/users', async (req, res) => {
+    try {
+        const users = await read()
+        res.json(users);
+     }catch (e){
+        res.status(500).json(e.message);
+    }
+    })
 
+app.post('/users', async (req, res) => {
+   try {
+       if (!req.body.name || req.body.name.length < 3) {
+           return res.status(400).json('Name is required and should be minimum 3 symbols');
+       }
+       if (!req.body.email || !req.body.email.includes('@') ) {
+           return res.status(400).json('Email is required');
+       }
+       if (!req.body.password || req.body.password.length < 8) {
+           return res.status(400).json('Password is required and should be minimum 8 symbols');
+       }
+    const users = await read();
+    const newUser = {
+        id: users.length ? users[users.length - 1].id + 1 : 1,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }
+    users.push(newUser);
+    await write(users);
+    res.status(201).json(newUser);
+ }catch (e){
+    res.status(500).json(e.message);
+}
+})
+app.get('/users/:userId', async (req, res) => {
+   try {
+    const users = await read();
+    const user = users.find(user => user.id === +req.params.userId);
+    res.json(user);
+}catch (e){
+    res.status(500).json(e.message);
+}
+})
+
+
+
+app.put('/users/:userId', async (req, res) => {
+
+
+    try {
+        if (!req.body.name || req.body.name.length < 3) {
+            return res.status(400).json('Name is required and should be minimum 3 symbols');
+        }
+        if (!req.body.email || !req.body.email.includes('@') ) {
+            return res.status(400).json('Email is required');
+        }
+        if (!req.body.password || req.body.password.length < 8) {
+            return res.status(400).json('Password is required and should be minimum 8 symbols');
         }
 
+        const users = await read();
+        const index = users.findIndex(user => user.id === Number(req.params.userId));
+
+        if(index === -1 ){
+            return res.status(404).send('User not found.');
+        }
+
+
+        const  user = users[index]
+            user.name= req.body.name;
+            user.email = req.body.email;
+            user.password = req.body.password;
+
+        await write(users);
+        res.status(201).json(users);
+
+    }catch (e){
+        res.status(500).json(e.message);
+
     }
+})
 
+
+
+app.delete('/users/:userId', async (req, res) => {
+ try {
+
+     const users = await read();
+    const index  = users.findIndex(user => user.id === +req.params.userId);
+
+     if(index === -1 ){
+         return res.status(404).send('User not found.');
+     }
+     console.log(users);
+     users.splice(index, 1);
+
+     await write(users);
+    res.sendStatus(204);
+
+}catch (e){
+    res.status(500).json(e.message);
 }
-    void foo();
+})
 
+
+
+
+
+
+const port = process.env.PORT;
+app.listen(port,()=>{
+console.log(`Listening on port ${port}`);
+})
